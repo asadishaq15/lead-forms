4255a4c396c0cd423363836c99335144def847d8
asadishaq15 <asadishaq392@gmail.com>
2025-03-05 22:14:29 +0500

    add new form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import KBBLeadForm from './Components/KBBForm';
 import AdvanceGrowMarketingLeadForm from './Components/AdvanceGrowForm';
+import AdvanceGrowMarketingCallTransferForm from './Components/AdvanceGrow2';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<KBBLeadForm />} />
         <Route path="/growx" element={<AdvanceGrowMarketingLeadForm />} />
+        <Route path="/leads" element={<AdvanceGrowMarketingCallTransferForm />} />
    
       </Routes>
     </Router>
