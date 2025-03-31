487ce40be63c4e26d7ad66a80dd3cdd978eb051a
asadishaq15 <asadishaq392@gmail.com>
2025-04-01 01:04:50 +0500

    add new form

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import KBBLeadForm from './Components/KBBForm';
 import AdvanceGrowMarketingLeadForm from './Components/AdvanceGrowForm';
 import AdvanceGrowMarketingCallTransferForm from './Components/AdvanceGrow2';
 import AdvanceGrowMarketingLeadForm2 from './Components/AdvanceGrow3';
+import KBBSalesGroupLeadForm2 from './Components/KBBForm2';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/growx" element={<AdvanceGrowMarketingLeadForm />} />
         <Route path="/growx2" element={<AdvanceGrowMarketingLeadForm2 />} />
         <Route path="/leads" element={<AdvanceGrowMarketingCallTransferForm />} />
+        <Route path="/leads2" element={<KBBSalesGroupLeadForm2 />} />
    
       </Routes>
     </Router>
